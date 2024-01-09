f0d694913883c4d49f1f7e0899417a29a8581781
AliHamza2317 <alihamza707580@gmail.com>
2024-01-10 04:01:14 +0500

    refactor-like.jsx

--- client/src/components/post/Like.jsx
+++ client/src/components/post/Like.jsx
@@ -1,33 +1,29 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { HiOutlineHandThumbUp, HiHandThumbUp } from "react-icons/hi2";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  likePostAction,
-  unlikePostAction,
-} from "../../redux/actions/postActions";
+import { likePostAction, unlikePostAction } from "../../redux/actions/postActions";
 
 const Like = ({ post }) => {
   const dispatch = useDispatch();
-  const { _id, likes } = post;
   const userData = useSelector((state) => state.auth?.userData);
 
-  const [likeState, setLikeState] = useState({
-    liked: post.likes.includes(userData._id),
-    localLikes: likes.length,
-  });
+  const initialLikeState = {
+    liked: post.likes.includes(userData?._id),
+    localLikes: post.likes.length,
+  };
+
+  const [likeState, setLikeState] = useState(initialLikeState);
 
   useEffect(() => {
     setLikeState({
-      liked: post.likes.includes(userData._id),
+      liked: post.likes.includes(userData?._id),
       localLikes: post.likes.length,
     });
-  }, [post.likes, userData._id]);
+  }, [post.likes, userData?._id]);
 
   const toggleLike = async (e) => {
     e.preventDefault();
-    const optimisticLikes = likeState.liked
-      ? likeState.localLikes - 1
-      : likeState.localLikes + 1;
+    const optimisticLikes = likeState.liked ? likeState.localLikes - 1 : likeState.localLikes + 1;
 
     setLikeState((prevState) => ({
       ...prevState,
@@ -37,9 +33,9 @@ const Like = ({ post }) => {
 
     try {
       if (likeState.liked) {
-        dispatch(unlikePostAction(_id));
+        await dispatch(unlikePostAction(post._id));
       } else {
-        dispatch(likePostAction(_id));
+        await dispatch(likePostAction(post._id));
       }
     } catch (error) {
       setLikeState((prevState) => ({
@@ -51,10 +47,7 @@ const Like = ({ post }) => {
   };
 
   return (
-    <button
-      onClick={toggleLike}
-      className="flex items-center cursor-pointer gap-1 text-lg"
-    >
+    <button onClick={toggleLike} className="flex items-center cursor-pointer gap-1 text-lg">
       {likeState.liked ? (
         <HiHandThumbUp className="text-2xl" />
       ) : (
